e3183fcfeacc098b1839af739f3a0e17cda24638
KevinD <KevinIDJ18@gmail.com>
2021-11-09 23:39:16 -0600

    Agregué pantalla de 'admin'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Own app Components
+import { UserViewComponent } from './user-view/user-view.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+
+
+// Material theme imports
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminViewComponent,
+    UserViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
